c82eb76dae8ccbbe06dfd0184eb31ead59e0e131
carloslopezjr <85579815+carloslopezjr@users.noreply.github.com>
2023-12-06 23:45:08 -0600

    Add functions in source files

--- functions.h
+++ functions.h
@@ -1,10 +1,16 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+void RemoveNewLine(char *stringInput);
 
+void Header();
 
+// int decision(int counter);
 
+void Logger();
 
+void MenuOptions();
 
+void LogIn();
 
 #endif
