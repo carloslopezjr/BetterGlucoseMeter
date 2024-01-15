bf66a32fb9512e089c425cbb39cac93ce7371475
carloslopezjr <85579815+carloslopezjr@users.noreply.github.com>
2024-01-14 23:27:47 -0600

    new comments for future updates

--- main.c
+++ main.c
@@ -73,19 +73,18 @@ int main()
         // Also it looks prettier when you open the file outside the program
 
 // 3: (UNDER CONSTRUCTION) - Working on it 1/12/24 
-// add insert function to insert a previous log from a different day and a deletion function
 
-    // Read the csv data and put it into an array.
+    // Read the csv data and put it into an array of structs: (COMPLETE)
         // We want to do this because if we have multiple iterations, we would need to read it anyways
 
-
     // This would allow me to do binary search when searching for specific data and other searching algorithms
-    
 
-// Fix Logger() logic to improve user experience
+    // add insert function to insert a previous log from a different day and a deletion function
+
 
 // 4: (FUTURE IMPLEMENTATION)
 // change meal tags to be more specific
+// Fix Logger() logic to improve user experience
 
 // 5: (FUTER IMPLEMENTATION)
 // ideate how to create "Calculate Insulin Dose" option for program
